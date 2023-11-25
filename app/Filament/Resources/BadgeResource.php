554137fc6b533aaca03da5baeb20d9bf1187ad34
basiltap <basiltap@hotmail.com>
2023-11-25 23:13:51 +0100

    first commit badge

---
+++ app/Filament/Resources/BadgeResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BadgeResource\Pages;
+use App\Filament\Resources\BadgeResource\RelationManagers;
+use App\Models\Badge;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\Alignment;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BadgeResource extends Resource
+{
+    protected static ?string $model = Badge::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('badge'),
+                Tables\Columns\TextColumn::make('text1'),
+                Tables\Columns\TextColumn::make('text2'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+//                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make()->color('info'),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make([
+                    TextEntry::make('badge')
+                        ->alignment(Alignment::Center)
+                        ->label(false)
+                        ->badge()
+                        ->color('success'),
+
+                    TextEntry::make('text1')
+                        ->alignment(Alignment::Center)
+                        ->label(false),
+
+                    TextEntry::make('text2')
+                        ->alignment(Alignment::Center)
+                        ->label(false),
+                ])
+                    ->columnSpan(1),
+
+            ])
+            ->columns(3);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBadges::route('/'),
+            'create' => Pages\CreateBadge::route('/create'),
+            'edit' => Pages\EditBadge::route('/{record}/edit'),
+        ];
+    }
+}
